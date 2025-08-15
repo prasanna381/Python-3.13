ea267e6eb39d5c23f3828608c1fc0bacdb2ae7f2
prasanna381 <boddusri123a@gmail.com>
2025-08-15 17:27:02 +0530

    operators.py

---
+++ operators.py
@@ -0,0 +1,11 @@
+name="sneha"
+age =43
+new_age=45
+print(f"my {name} and iam {age} older then you")
+print(f"my {name} and iam {age+new_age} older then you")
+print(f"my {name} and iam {age-new_age} older then you")
+print(f"my {name} and iam {age*new_age} older then you")
+print(f"my {name} and iam {age/new_age} older then you")
+print(f"my {name} and iam {age%new_age} older then you")
+print(f"my {name} and iam {age&new_age} older then you")
+print(f"my {name} and iam {age==new_age} older then you")
