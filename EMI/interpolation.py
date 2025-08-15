c93ebf87383d5dd713c5c2db466eb89b03e9dfc3
prasanna381 <boddusri123a@gmail.com>
2025-08-15 17:05:27 +0530

    interpolation

---
+++ EMI/interpolation.py
@@ -0,0 +1,11 @@
+name = "Priya"
+fname="rahul"
+mname="keerthi"
+age  = 22
+fage=40
+mage=38
+
+print (f"my name is{name}and my{age}older")
+print(f"my father{fname}he is{fage}older")
+print (f"my mother{mname}andher{mage}older")
+
